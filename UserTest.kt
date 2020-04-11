c4868ea2e3665820802f195baf34631132a86e09
Prasanth496 <60653955+Prasanth496@users.noreply.github.com>
2020-04-11 12:16:38 +0530

    [prasanth]refactor. implemented test for reference check in program

--- UserTest.kt
+++ UserTest.kt
@@ -6,10 +6,19 @@ import org.junit.Test
 
 class UserTest {
     @Test
+    // function for null check
     fun givenUserOneAndNull_WhenEquals_ShouldNotBeEqual() {
-        val UserOne = User(name = "Pavan",mobileNumber = "9432567834")
-        val UserTwo = null
+        val userOne = User(name = "Pavan",mobileNumber = "9432567834")
+        val userTwo = null
         Assert.assertFalse(UserOne == UserTwo)
+
+    }
+
+    @Test
+    // function for reference check
+    fun givenSameUser_WhenEquals_ShouldBeEqual() {
+        val userOne = User(name = "Pavan",mobileNumber = "9432567834")
+        Assert.assertTrue(userOne.equals(userOne))
 
     }
 }
