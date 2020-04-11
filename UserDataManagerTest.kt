1478ae272f95e80d27737109e40a84b600e05881
Prasanth496 <60653955+Prasanth496@users.noreply.github.com>
2020-04-11 14:49:33 +0530

    [prasanth]refactor. implemented test for add User in program

--- UserDataManagerTest.kt
+++ UserDataManagerTest.kt
@@ -2,6 +2,7 @@ package com.example.userlist.model
 
 import com.example.model.User
 import com.example.model.UserDataManager
+import org.junit.Assert
 import org.junit.Test
 
 class UserDataManagerTest {
@@ -12,4 +13,11 @@ class UserDataManagerTest {
         userDatamanager.create(user)
     }
 
+    @Test
+    fun ShouldBeableToAddOneUser() {
+        val userDatamanager = UserDataManager()
+        val user = User("Pavan",mobileNumber = "9543216423")
+        userDatamanager.add(user)
+    }
+    
 }
