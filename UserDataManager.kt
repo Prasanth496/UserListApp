5654f36c37ad90c2e464af971318c57e5da59c43
Prasanth496 <60653955+Prasanth496@users.noreply.github.com>
2020-04-11 17:16:24 +0530

    [prasanth]add. implemented UserDataManager.kt program

---
+++ UserDataManager.kt
@@ -0,0 +1,26 @@
+package com.example.model
+
+import java.util.ArrayList
+
+class UserDataManager {
+
+    fun create(user : User) : Boolean
+    {
+        return true
+    }
+
+    fun add(user: User) : Boolean
+    {
+        return true
+    }
+
+    fun delete(user: User) : Boolean
+    {
+        return true
+    }
+
+    fun display(user: User) : Boolean
+    {
+        return true
+    }
+}
