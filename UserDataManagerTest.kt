b01c184dc85c0b514def98f8ecc2beb2459b3734
Prasanth496 <60653955+Prasanth496@users.noreply.github.com>
2020-04-14 12:24:39 +0530

    [prasanth]add. added the insert one user test

--- UserDataManagerTest.kt
+++ UserDataManagerTest.kt
@@ -1,40 +1,17 @@
 package com.example.userlist.model
 
 import com.example.model.User
-import com.example.model.UserDataManager
+import com.example.model.UserDataManagerImpl
 import org.junit.Assert
 import org.junit.Test
 
 class UserDataManagerTest {
-    @Test
     // function test for create user
-    fun ShouldBeableToCreateOneUser() {
-        val userDatamanager = UserDataManager()
-        val user = User("Pavan",mobileNumber = "9543216423")
-        userDatamanager.create(user)
-    }
-
     @Test
-    // function test for add user
-    fun ShouldBeableToAddOneUser() {
-        val userDatamanager = UserDataManager()
-        val user = User("Pavan",mobileNumber = "9543216423")
-        userDatamanager.add(user)
-    }
+    fun ShouldBeableToInsertOneUser() {
+        val userDatamanager: UserDataManagerImpl = UserDataManagerImpl()
+        val user = User("Pavan", mobileNumber = "9543216423")
+        Assert.assertTrue(userDatamanager.insertUser(user))
 
-    @Test
-    // function test for delete user
-    fun ShouldBeAbleToDeleteUser() {
-        val userDatamanager = UserDataManager()
-        val user = User("Pavan",mobileNumber = "9543216423")
-        userDatamanager.delete(user)
-    }
-
-    @Test
-    // function test for display user
-    fun ShouldBeAbleToDisplayUser() {
-        val userDatamanager = UserDataManager()
-        val user = User("Pavan",mobileNumber = "9543216423")
-        userDatamanager.display(user)
     }
 }
