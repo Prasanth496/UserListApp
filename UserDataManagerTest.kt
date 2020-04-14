76a0907eb63cd5b1675df25d8f2fa25769d6099b
Prasanth496 <60653955+Prasanth496@users.noreply.github.com>
2020-04-14 13:11:17 +0530

    [prasanth]add. added test for insert two users

--- UserDataManagerTest.kt
+++ UserDataManagerTest.kt
@@ -11,7 +11,19 @@ class UserDataManagerTest {
     fun ShouldBeableToInsertOneUser() {
         val userDatamanager: UserDataManagerImpl = UserDataManagerImpl()
         val user = User("Pavan", mobileNumber = "9543216423")
-        Assert.assertTrue(userDatamanager.insertUser(user))
+        val userListSizeBeforeAdd = userDatamanager.readUsers().size
+        Assert.assertTrue(userListSizeBeforeAdd + 1, userDatamanager.readUsers().size)
+
+    }
+    @Test
+    fun ShouldBeableToInsertTwoUsers() {
+        val userDatamanager: UserDataManagerImpl = UserDataManagerImpl()
+        val userOne = User("Pavan", mobileNumber = "9543216423")
+        val userTwo = User("Ravi", mobileNumber = "9645432532")
+        val userListSizeBeforeAdd = userDatamanager.readUsers().size
+        userDatamanager.insertUser(userOne)
+        userDatamanager.insertUser(userTwo)
+        Assert.assertEquals(userListSizeBeforeAdd + 2, userDatamanager.readUsers().size)
 
     }
 }
